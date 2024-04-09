29e22f3f03bdb3238f918ac871c10539cf21f511
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-04-09 16:50:47 +0500

    card ui basic completed

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,470 @@
+import "./card.css";
+import { TbShoppingBagPlus } from "react-icons/tb";
+
+export const Card = () => {
+  return (
+    <section className="card">
+      <div className="container">
+        <div className="card-inner">
+          <h2 className="card-title">Products</h2>
+
+          <ul className="card-list">
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="card-item">
+              <img
+                src="https://picsum.photos/200/200"
+                alt="product"
+                className="card-img"
+                width={200}
+                height={250}
+              />
+              <div className="card-inner-box">
+                <h4 className="card-item-title">for men</h4>
+                <p className="card-text">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, excepturi.
+                </p>
+                <div className="card-bottom">
+                  <div className="card-bottom-left">
+                    <p className="card-bottom-deleted">100 000 so'm</p>
+                    <p className="card-bottom-text">50 000 so'm</p>
+                  </div>
+
+                  <button className="card-btn">
+                    <TbShoppingBagPlus />
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
+          <div className="card-btn-box">
+          <button className="card-bottom-btn">20 more views</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Layouts } from '../../components/Layouts'
 import { Hero } from '../../components/Hero'
+import { Card } from '../../components/Card'
 
 export const Home = () => {
   return (
     <Layouts>
       <Hero/>
+      <Card/>
     </Layouts>
   )
 }
