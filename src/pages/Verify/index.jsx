2a5a73b23fccc12f9b8682397fffa9128f1c7289
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-07-13 11:35:14 +0500

    verify page made

--- src/pages/Verify/index.jsx
+++ src/pages/Verify/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Container, Title } from "../../components/StyledComponents";
+import "./verify.css";
+import { HStack, PinInput, PinInputField } from '@chakra-ui/react'
 
 export const Verify = () => {
   return (
-    <div>Verify</div>
-  )
-}
+    <Container>
+      <Title className="verify-title">Verify code</Title>
+      <p className="verify-text">
+        You must enter a verification code. Check your email for the code.
+      </p>
+      <HStack className="verify-input-wrapper">
+        <PinInput>
+          <PinInputField className="verify-input"/>
+          <PinInputField className="verify-input"/>
+          <PinInputField className="verify-input"/>
+          <PinInputField className="verify-input"/>
+          <PinInputField className="verify-input"/>
+          <PinInputField className="verify-input"/>
+        </PinInput>
+      </HStack>
+    </Container>
+  );
+};
