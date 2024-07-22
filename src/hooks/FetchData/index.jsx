cc0b9d76b2fa6de08acba9d642f680a39bd6a9d2
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-07-22 14:09:03 +0500

    added custom hook call api

---
+++ src/hooks/FetchData/index.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [data, loading, error];
+};
