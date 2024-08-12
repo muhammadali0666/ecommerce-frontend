16e9275c3662b67a8475c856ad9b5814246f1770
muhammadaliprogrammer <shuhratjonovmuhammadali8@gmail.com>
2024-08-12 17:07:53 +0500

    added

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,12 +5,24 @@ import { TbShoppingBagPlus } from "react-icons/tb";
 import { Box, CircularProgress } from "@mui/material";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useFetch } from "../../hooks/FetchData";
 
 const notify = (info) => toast.warn(info);
 
 export const Card = () => {
   const [allProducts, setAllProducts] = useState([]);
   const [pageCount, setPageCount] = useState(20);
+
+  // const [data, loading, error] = useFetch('http://localhost:4001/all_products');
+
+  // if (loading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
+
 
   const fetchLatestProducts = (pageCount) => {
     fetch(`http://localhost:4001/all_products`, {
@@ -52,10 +64,6 @@ export const Card = () => {
           }
         });
     }
-  };
-
-  const shoppingCart = (evt, id) => {
-    evt.preventDefault();
   };
 
   return (
